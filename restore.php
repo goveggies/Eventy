09a1463a349c572a6c088639dd5f807bd1a2a618
Guillaume Bonnet <giomebs@gmail.com>
2018-04-17 16:35:26 +0200

    version la plus récente

--- restore.php
+++ restore.php
@@ -1,28 +1,58 @@
 <?php
 
-// trois états 
+// trois états
 
 // récupération du token, si y'a token on traite, sinon, on affiche la page de base
 // l'adresse mail existe pas, on dit que c'est une arnaque.
 // l'adresse mail existe, on envoie l'email et on affiche un message comme quoi ça a été envoyé.
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 $etat = 'initial';
 
-if(isset($_GET('t')) {
-    // token est initialisé
+if(isset($_GET['t'])) {
+    // token est initialisé, on permet le changement de mpd
+
+
+
+
     $etat = 'token';
 } else {
     // token non initialité
     if(isset($_POST['submitmail'])) {
         // mail submitté
-        
+        $destinataire = $_POST['submitmail'];
+        require('connect.php');
+
         /*
             vérifier si le mail existe dans la bdd
-            si il n'existe pas : on active l'erreur
-            si il existe : on envoie un mail en générant toekn
+            si il n'existe pas : on change l'état (mailexistepas), et on active l'erreur
+            si il existe : on génère token on envoie mail
         */
+
+        $token = bin2hex(random_bytes($length));
+
+        // rentrer le token dans la base de donnée
+
+
+
+        // si il existe
+        $expediteur = 'mot-de-passe-perdu@eventy.com';
+        $headers  = 'MIME-Version: 1.0' . "\n"; // Version MIME
+        $headers .= 'Content-type: text/html; charset=ISO-8859-1'."\n"; // l'en-tete Content-type pour le format HTML
+        $headers .= 'Reply-To: '.$expediteur."\n"; // Mail de reponse
+        $headers .= 'From: "Nom_de_expediteur"<'.$expediteur.'>'."\n"; // Expediteur
+        $headers .= 'Delivered-to: '.$destinataire."\n"; // Destinataire
+        $headers .= 'Cc: '.$copie."\n"; // Copie Cc
+        $headers .= 'Bcc: '.$copie_cachee."\n\n"; // Copie cachée Bcc
+
+        $sujet = "Eventy - Restaurer son mot de passe. ";
+        $message ="On change votre mail sur cette adresse: http://eventy.com/?t=" . $token;
+
+        if(mail($destinataire,$sujet,$message,$headers)) {
+          // mail bien envoyé
+        } else {
+          // mail mal envoyé
+          $etat = 'erreurMail';
+        }
     } else {
         // aucun mail submitté.
         echo 'pas de mail submitté';
@@ -34,7 +64,7 @@ if(isset($_GET('t')) {
 
 
 // mot de passe bien initialisé, on envoie sur la page pour dire que c'est bon, avec lien avec connnexion
-// mot de passe mal initialisé, par similaire, on reload la page de restauration 
+// mot de passe mal initialisé, par similaire, on reload la page de restauration
 
 ?>
 
@@ -45,44 +75,48 @@ if(isset($_GET('t')) {
 <head>
     <meta charset="UTF-8">
     <title>Eventy - Restaurer un mot de passe</title>
-    
+
     <link rel="stylesheet" href="style/screen.css">
     <link rel="icon" type="image/png" href="chick.png" />
     <link href="https://fonts.googleapis.com/css?family=Gugi" rel="stylesheet">
 </head>
 <body>
     <div id="inscription">
-        
-        
+
+
         <?php
-        
+
         if($etat == 'initial') {
-            ?>    
+            ?>
                <h4 style="text-align:center; font-size: 2em;">Mot de passe disparu de vostre cap ? </h4>
                 <form action="restore.php" method="post">
                     <label for="adressemail">Adresse mail</label>
                     <input type="email" name="adressemail"><br>
                     <input type="submit" name="submitmail" value="Récupération">
-                </form>	
+                </form>
                 <p class="center">Si l'adresse mail existe, on retourne vers vous ! </p>
             <?php
         } else if ($etat == 'mailexiste') {
             ?>
-            
+
             <p class="center">On vient de vous envoyer un mail sur <?php echo $mail; ?>. Le processus est lance. Ouvrez votre boite mail.</p>
-	    
+
             <?php
         } else if ($etat == 'mailexistepas') {
             ?>
-              
-            <p class="center">Votre email n'existe pas dans notre dimension, etes-vous sur d'exister chez nous ?</p>  
-            
+
+            <p class="center">Votre email n'existe pas dans notre dimension, etes-vous sur d'exister chez nous ?</p>
+
             <?php
+        } else if($etat =='erreurMail') {
+          ?>
+          <p class="center">L'email s'est mal envoyé. Il y a eu une erreur, re-essayez.</p>
+          <?php
         } else if ($etat == 'token') {
-            
+
             // faire la recherche sql
             ?>
-            
+
              <h4 style="text-align:center; font-size: 2em;"> Parametrez un nouveau mot de passe </h4>
              <form action="restore.php" method="post">
                 <label for="motdepasse">Nouveau mot de passe </label>
@@ -91,18 +125,18 @@ if(isset($_GET('t')) {
                 <input type="motdepasse_verif" name="adressemail"><br>
                 <input type="submit" name="submitpass" value="Procéder">
             </form>
-            
+
             <?php
         }
-   
-   
+
+
         ?>
-        
-        
-       
-       
-	   
-        
+
+
+
+
+
+
     </div>
 </body>
 </html>
