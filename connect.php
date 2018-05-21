84539b19a7eb94eeea361c413a8a0700f166d66e
Guillaume Bonnet <giomebs@gmail.com>
2018-05-21 10:04:39 +0200

    Testing .gitignore

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$linkdpo = new PDO("mysql:host=...;dbname=...", "...", "...");
+		$linkdpo = new PDO("mysql:host=...;dbname=bla", "...", "...");
 	}catch(Exception $e){
 		die('Erreur: '.$e->getMessage());
 	}
